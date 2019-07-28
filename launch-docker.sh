8ea729d25bb1af7310fdede66be20b82652c7bea
flottin <flotitn@gmail.com>
2019-07-29 00:08:28 +0200

    add config

--- launch-docker.sh
+++ launch-docker.sh
@@ -5,4 +5,8 @@ docker build -t eg_sshd .
 
 docker run -d -P -p 22:22 -p 80:80 --name test_sshd_80 eg_sshd
 docker run -d -P -p 23:22 -p 81:80 --name test_sshd_81 eg_sshd
-docker run -d -P -p 3306:3306 --name test_sshd_3306 eg_sshd
+docker run -d -P -p 24:22 --name test_sshd_3306 eg_sshd
+
+$ docker container stop test_sshd
+$ docker container rm test_sshd
+$ docker image rm eg_sshd
