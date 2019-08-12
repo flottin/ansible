e89d11792c0c5239e277463fc09e9a6519b69d62
Florent LOTTIN <flottin@gmail.com>
2019-08-12 05:08:22 +0200

    add redis

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -1,11 +1,10 @@
 <html>
-
 <style>
 
     html{
         font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-        background: black;
-        color: darkgoldenrod;
+
+        color: gray;
     }
     span{
         font-weight: bold;
@@ -13,15 +12,31 @@
     }
 
 </style>
-
 </html>
 
-
 <?php
+file_put_contents('data.txt', 'all datas');
+
+$config = [
+    'module1' => [
+        'param1' => 'val 1',
+        'param2' => 'val 2',
+        'param3' => 'val 3',
+        'param4' => 'val 4',
+    ],
+
+    'module2' => [
+        'param1' => 'val 1',
+        'param2' => 'val 2',
+        'param3' => 'val 3',
+        'param4' => 'val 4',
+    ]
+];
+file_put_contents('data-json.txt', json_encode($config) );
 
 $separatorSize = 120;
-
-echo "<br>\n";
+echo '<pre>';
+echo "\n";
 echo str_repeat('-', $separatorSize);
 echo "<br>\n";
 echo '<span>Deploy with ansible : ' . $_SERVER['HTTP_HOST'] . '</span>';
@@ -35,10 +50,19 @@ foreach($_SERVER as $k => $v)
         echo "<br>\n";
     }
 }
-
 echo str_repeat('-', $separatorSize);
 echo "<br>\n";
-
-echo '<pre>';
+echo "Config";
+echo "<br>\n";
+echo str_repeat('-', $separatorSize);
+echo "<br>\n";
+var_dump(json_decode(file_get_contents('data-json.txt')));
+echo "<br>\n";
+echo str_repeat('-', $separatorSize);
+echo "<br>\n";
+echo "Global Variables";
+echo "<br>\n";
+echo str_repeat('-', $separatorSize);
+echo "<br>\n";
 var_dump($_SERVER);
 var_dump($_ENV);
